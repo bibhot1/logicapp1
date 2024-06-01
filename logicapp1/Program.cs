6dbf3295b942a021847eab69e10122a7b6b164dd
Bibhot1 <bndazbvu.me12@gmail.com>
2024-06-02 06:42:33 +1200

    Add project files.

---
+++ logicapp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("How old are you?");
+int age = int.Parse(Console.ReadLine());
+bool isWithParents = false;
+
+if(age > 18)
+{
+    Console.WriteLine("Go party in the club");
+}
+else if (age >= 13)
+
+{
+    Console.WriteLine("Are you with your parents press y for yes and n for no");
+    string isWithParentsString = Console.ReadLine();
+    if(isWithParentsString == "y")
+    {
+        Console.WriteLine("Party with parents");
+
+
+    }
+    else
+    {
+        Console.WriteLine("No party for you today");
+    }
+}
+else
+{
+    Console.WriteLine("sTAY AT HOME.");
+}
